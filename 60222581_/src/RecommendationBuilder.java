f1ebcfefadab2eabce89bdedf0b44d8a0e1a2b43
Sudoridorie <164470534+Sudoridorie@users.noreply.github.com>
2024-05-20 23:34:14 +0900

    Delete 60222581_/src directory

--- 60222581_/src/RecommendationBuilder.java
+++
@@ -1,7 +0,0 @@
-public interface RecommendationBuilder {
-    RecommendationBuilder setRecommendationID(String recommendationID);
-    RecommendationBuilder setUserID(String userID);
-    RecommendationBuilder setClothingItemID(String clothingItemID);
-    RecommendationBuilder setReason(String reason);
-    Recommendation build();
-}
