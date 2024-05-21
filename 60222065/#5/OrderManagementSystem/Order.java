13e6e3e61702f67d0fc3930ce1d8799e4e19b9a4
OrangeKim04 <120697629+OrangeKim04@users.noreply.github.com>
2024-05-21 12:12:40 +0900

    Update Order.java

--- 60222065/#5/OrderManagementSystem/Order.java
+++ 60222065/#5/OrderManagementSystem/Order.java
@@ -1,10 +1,19 @@
 package OrderManagementSystem;
 
 public class Order {
-	private String orderID;
+    private String orderID;
     private String status;
 
     public void setStatus(String status) {
         this.status = status;
     }	
+    public void setOrderID(String id) {
+    	this.orderID = id;
+    }
+    public String getOrderID() {
+    	return this.orderID;
+    }
+    public String getStatus() {
+    	return this.status;
+    }
 }
